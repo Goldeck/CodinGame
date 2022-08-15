c18aeba8ada26abe0df86bb0ae7737c116c70074
Younes EL JARRARI <y.eljarrari@cyberjet.fr>
2022-08-15 18:00:53 +0200

    first commit

---
+++ CodingGameTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CodingGame
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
